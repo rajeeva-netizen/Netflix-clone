292742ab23798c225c3fc1dad305f8e5305158ca
rajeeva-netizen <74817212+rajeeva-netizen@users.noreply.github.com>
2021-05-15 21:02:51 +0530

    fetched the data using the axios

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,11 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from './axios'
 
-function Row({title}){
-    const [movie, setMovies] = useState([])
+const base_url ='https://image.tmdb.org/t/p/original/'
+function Row({title, fetchUrl}){
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            // console.log(request.data.results);
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData();
+    }, [fetchUrl]);
+    console.table(movies)
     return(
-        <>
+        <div className='row'>
             <h2>{title}</h2>
-        </>
+            <div className='posters'>
+                {
+                    movies.map((movie, index)=>
+                    <img src={`${base_url}${movie.backdrop_path}`} alt={movie.name}/>
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
